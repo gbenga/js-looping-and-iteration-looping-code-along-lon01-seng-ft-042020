f51e83f3c575e40c4126373ae41a40e30d7148ec
gojoaromokudu <gojoaromokudu@gmail.com>
2020-06-02 23:44:02 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// const names1 = ["Ada", "Brendan", "Ali"]
+
+function writeCards(names,day) {
+    let messages = []
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${day} gift!`);
+
+    }
+    return messages
+}
+
+function countDown(num){
+    let i = num;
+    while (i > -1) {
+        console.log(i);
+        i--;
+    }
+}
